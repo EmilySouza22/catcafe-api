e4ed40a79ddeda37d0ca4c910bbe59bb65335b6a
EmilySouza22 <emilyseafonso@gmail.com>
2026-06-08 19:40:52 -0300

    feat: autenticação admin

---
+++ api/src/middlewares/admin.js
@@ -0,0 +1,9 @@
+// verificando e autorizando o admin
+const AuthAdmin = (req, res, next) => {
+    if (!req.user || req.user.role !== 'admin') {
+        return res.status(403).json({ success: false, message: 'Acesso negado' });
+    }
+    next();
+};
+
+module.exports = AuthAdmin;
